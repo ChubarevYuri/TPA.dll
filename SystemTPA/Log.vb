29828ae7231e1628b6baf5821bcb51fe7943728e
ChubarevYuri <chubarev.yuri@mail.ru>
2022-08-08 16:13:06 +0600

    убил жука

--- SystemTPA/Log.vb
+++ SystemTPA/Log.vb
@@ -94,13 +94,13 @@
                 If rank >= printRank Then
                     SizeControl()
                     Dim text As String = rank.ToString().PadRight(8) & DateAndTime.Now.ToString("yyyyMMddHHmmss") & " " & message
-                    Using f As New System.IO.StreamWriter(fileAddress, True)
-                        Try
+                    Try
+                        Using f As New System.IO.StreamWriter(fileAddress, True)
                             f.WriteLine(text)
                             f.Close()
-                        Catch ex As Exception
-                        End Try
-                    End Using
+                        End Using
+                    Catch ex As Exception
+                    End Try
                 End If
             End If
         End If
